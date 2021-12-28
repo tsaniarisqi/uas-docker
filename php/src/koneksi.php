d4ec10727dcef9a5376ef9aab425cfe9edb42b12
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-12-28 13:38:29 +0700

    update

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', '172.20.0.2');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'admin');
 define('DB_NAME', 'db_karyawan');
 
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
